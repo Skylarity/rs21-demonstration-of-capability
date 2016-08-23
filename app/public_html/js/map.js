782579fa0a5469009c6a7b1ea7ab58a2890410f8
Skyler Rexroad <skyrex1095@gmail.com>
2016-08-23 10:04:55 -0600

    Adjusted map to center on Bernalillo County

--- app/public_html/js/map.js
+++ app/public_html/js/map.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// Bernalillo: 35.0440093,-106.9530535
 	L.mapbox.accessToken = "pk.eyJ1Ijoic2t5bGFyaXR5IiwiYSI6ImNpczI4ZHBmbzAwMzgyeWxrZmZnMGI5ZXYifQ.1-jGFvM11OgVgYkz3WvoNw";
 	var map = L.mapbox.map("censusmap", "mapbox.streets");
-	map.setView([35.0440093, -106.9530535], 10);
+	map.setView([35.0178, -106.6291], 11);
 
 	var featureLayer = L.mapbox.featureLayer().loadURL('data/BernallioCensusBlocks_Joined.json').addTo(map);
 });
